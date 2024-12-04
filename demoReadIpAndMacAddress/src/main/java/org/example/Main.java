b10c945698c61f759c6df71800ec61f09f726fc7
linhduc2506 <sktteof@gmail.com>
2024-12-04 14:16:54 +0700

    upload project file

---
+++ demoReadIpAndMacAddress/src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // Lấy địa chỉ IP của máy
+            InetAddress ip = InetAddress.getLocalHost();
+            System.out.println("Địa chỉ IP: " + ip.getHostAddress());
+
+            // Lấy giao diện mạng dựa trên IP
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+
+            // Lấy địa chỉ MAC
+            byte[] mac = network.getHardwareAddress();
+
+            if (mac != null) {
+                System.out.print("Địa chỉ MAC: ");
+                StringBuilder macAddress = new StringBuilder();
+                for (int i = 0; i < mac.length; i++) {
+                    macAddress.append(String.format("%02X", mac[i]));
+                    if (i < mac.length - 1) {
+                        macAddress.append("-");
+                    }
+                }
+                System.out.println(macAddress.toString());
+            } else {
+                System.out.println("Không thể lấy địa chỉ MAC.");
+            }
+
+        } catch (UnknownHostException e) {
+            System.err.println("Không thể tìm thấy địa chỉ IP của máy.");
+        }
+    }
+}
